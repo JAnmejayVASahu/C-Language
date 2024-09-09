c326a6e1c991e644f6665a10fb8deee9baa59935
Janmejay Sahu <chikunsahu73@gmail.com>
2024-09-09 16:34:27 +0530

    Test 2

---
+++ Experements-1/Exp2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int x;
+    x = 10;
+    printf("x");// Here printf print x don't print 10 because when you ENTER the value in between double qoute printf print the sme valuse as it is.
+    printf("\n%d",x);// %d is represent to print the integer value.
+}
