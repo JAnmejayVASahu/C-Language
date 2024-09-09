84a664d2d9a265633a45a1c0a343103f19d09253
Janmejay Sahu <chikunsahu73@gmail.com>
2024-09-09 17:01:49 +0530

    Taking input from user & Find out the summeation of this number.

---
+++ Experements-1/Exp12.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    float num1, num2;
+    printf("Enter 1st number- ");
+    scanf("%f",&num1);
+    printf("\nEnter 2nd number- ");
+    scanf("%f",&num2);
+    float sum = (num2 + num1);
+    printf("\n%f",sum);
+}
