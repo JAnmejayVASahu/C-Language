a45a6384458f48934f1baaa8a11bad32da35b8de
Janmejay Sahu <chikunsahu73@gmail.com>
2024-09-09 16:29:13 +0530

    We are experimenting with the C language.

---
+++ Experements-1/Exp1.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("nn\n\nnn\n"); //\n which is represent to next line or Enter.
+    printf("\n/n/nnn/n");//Here this line print as it is because forward slash.
+}
