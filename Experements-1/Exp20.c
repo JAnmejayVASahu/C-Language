64e7f43096bf2e6f4b708a7f19500dc1167ea6bb
Janmejay Sahu <chikunsahu73@gmail.com>
2024-09-09 18:53:22 +0530

    ASSCII value

---
+++ Experements-1/Exp20.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    char js = 'a';
+    printf("%c", js);
+    printf("\n%d", js); // Alphabet(Capital,small) always maped with a number.
+    printf("\n%f", js);
+}
