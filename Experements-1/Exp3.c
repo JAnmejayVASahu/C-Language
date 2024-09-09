5fc39a89cf429dac1b11cf2cdb955ea5eda3391a
Janmejay Sahu <chikunsahu73@gmail.com>
2024-09-09 16:44:16 +0530

    Basic Arithmatics

---
+++ Experements-1/Exp3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int x = 56;    
+    printf("Still x value is-%d",x);
+    x = x + 4;
+    printf("\nAfter additon  value is-%d",x);
+    x = x - 10;
+    printf("\nAfter additon  value is-%d",x);
+    int y = 70;
+    y +=1;
+    printf("\nAfter 2nd additon  value is-%d",y);
+}
