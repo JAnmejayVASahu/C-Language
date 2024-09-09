dc1e7413d5a1c3b61b501e7a2120ec4f4ebb9e41
Janmejay Sahu <chikunsahu73@gmail.com>
2024-09-09 16:47:48 +0530

    volume of a surface uning int(We know that the volume of sphere is 4/3 pai "r"que)

---
+++ Experements-1/Exp6.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int r = 5.5;
+    float pai = 3.14;
+    float vloume = ((4*pai*(r*r*r)/3));// you can use the 3.14 in position of pai directly like this(4*3.14*(r*r*r)/3)
+    printf("%f",vloume);
+}
