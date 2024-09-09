8944d73ab438892e1f4e9b9af5bd8ebcaa28c9f6
Janmejay Sahu <chikunsahu73@gmail.com>
2024-09-09 16:52:11 +0530

    Calculating of persentage of 5 subject.

---
+++ Experements-1/Exp9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int x1 = 61; // x1 = Physics mark
+    int x2 = 54; // x2 = mathmatics mark
+    int x3 = 62; // x3 = Chemestry mark
+    int x4 = 96; // x4 = Voctional mark
+    int x5 = 75; // x5 = Mother language mark
+    int x6 = 54; // x1 = English mark
+    float x7 = (((x2 + x3 + x1 + x4 + x5 + x6) * 100) / 600);
+    printf("\nThis the mark of janmejay in 12th Exam- %f", x7);
+}
