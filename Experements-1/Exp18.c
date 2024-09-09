ec6009d6706e493f9a5e00bd56a5e5954870d38d
Janmejay Sahu <chikunsahu73@gmail.com>
2024-09-09 18:50:05 +0530

    added of format

--- Experements-1/Exp18.c
+++ Experements-1/Exp18.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-int main(){
-    int i = 2*3/4+4/4+8-2+5/8;
-    printf("your answer is %d.",i);
+#include <stdio.h>
+int main()
+{
+    int i = 2 * 3 / 4 + 4 / 4 + 8 - 2 + 5 / 8;
+    printf("your answer is %d.", i);
 }
