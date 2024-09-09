a6abbb7a50b7fb2b1342e8c46b4bfb55f9dce796
Janmejay Sahu <chikunsahu73@gmail.com>
2024-09-09 19:21:39 +0530

    Take positive integer input & tell if it is divisible by 5 & 3.

---
+++ Experements-3 (Conditional Statement)/Exp2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int div;
+    printf("Enter a Number: ");
+    scanf("%d", &div);
+    if (div % 5 == 0 && div % 3 == 0)
+    {
+        printf("%d is divisable by 5 & 3", div);
+    }
+    else
+    {
+        printf("%d is not divisable by 5 & 3", div);
+    }
+}
