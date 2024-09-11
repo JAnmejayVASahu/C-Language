69e4a03099d7c152e85ab00ddd71d76bb15fc9f3
Janmejay Sahu <chikunsahu73@gmail.com>
2024-09-11 21:09:58 +0530

    Do....while loop(Difference between do...while or while loop)

---
+++ Experements-7 (Loops)/Exp26.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int x = 1;
+    // while(x<10){
+    //     printf("Hello\n");
+    //     x++;
+    // }
+    do
+    {
+        printf("Hello\n");
+        x++;
+    } while (x < 10);
+}
